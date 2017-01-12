f10bccdedc03e2c155e8f4bc64c1400f7256173a
Turner VE <Turner VE>
2017-01-12 09:17:09 -0500

    added switch and test

---
+++ 0.08_switch.js
@@ -0,0 +1,22 @@
+//switch Cases
+
+var marginOfColtSuperBowlWins = 17
+var result
+
+switch (marginOfColtSuperBowlWins) {
+	case 3:
+		result = "Cool, that will be a good game."
+		break;
+	case 7:
+		result = "I like it, just close enough."
+		break;
+	case 10:
+		result = "That's what I'm talking about."
+		break;
+	case 17:
+		result = "Sweet, we're awesome."
+		break;
+	default: = "mmhmm getting there"
+}
+
+console.log("Switch Result: " + result)
